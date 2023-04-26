6f8b46df634a0440e718ecbd00d58b34d2f438f8
sook <sukkyu.chung@gmail.com>
2023-04-26 15:00:55 +0900

    update data

--- app/[version]/[region]/[league]/loading.tsx
+++
@@ -1,8 +0,0 @@
-// https://beta.nextjs.org/docs/routing/loading-ui#thread-id=9V4--
-export default function Loading() {
-  return (
-    <div className="animate-pulse fixed inset-0 flex justify-center items-center text-4xl">
-      {"Loading....".toUpperCase()}
-    </div>
-  );
-}
